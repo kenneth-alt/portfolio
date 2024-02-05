47a153b1dab5486f14a5dcd19b7ee675799e41f9
kenneth-alt <kenneth.ugo15@gmail.com>
2024-02-05 00:35:00 -0700

    add NavigationContext

---
+++ src/app/context/NavigationContext.tsx
@@ -0,0 +1,55 @@
+import React, { createContext, useRef, ReactNode, useContext } from 'react';
+
+interface NavigationContextType {
+  homeRef: React.RefObject<HTMLDivElement>;
+  aboutRef: React.RefObject<HTMLDivElement>;
+  skillsRef: React.RefObject<HTMLDivElement>;
+  projectsRef: React.RefObject<HTMLDivElement>;
+  blogsRef: React.RefObject<HTMLDivElement>;
+  contactRef: React.RefObject<HTMLDivElement>;
+}
+
+// Create the context with an initial undefined value to enforce provider usage
+const NavigationContext = createContext<NavigationContextType | undefined>(
+  undefined
+);
+
+interface NavigationProviderProps {
+  children: ReactNode;
+}
+
+// NavigationProvider component with children of type ReactNode
+export const NavigationProvider = ({ children }: NavigationProviderProps) => {
+  const homeRef = useRef<HTMLDivElement>(null);
+  const aboutRef = useRef<HTMLDivElement>(null);
+  const skillsRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const blogsRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  // The value that will be passed to the context consumers
+  const value: NavigationContextType = {
+    homeRef,
+    aboutRef,
+    skillsRef,
+    projectsRef,
+    blogsRef,
+    contactRef,
+  };
+
+  return (
+    <NavigationContext.Provider value={value}>
+      {children}
+    </NavigationContext.Provider>
+  );
+};
+
+export function useNavigationContext() {
+  const context = useContext(NavigationContext);
+  if (context === undefined) {
+    throw new Error(
+      'useNavigationContext must be used within a NavigationProvider'
+    );
+  }
+  return context;
+}
